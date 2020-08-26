a7d9f6562bc5eecb6914dd1ab6105b8227b0919e
Aayushi Bansal <aayushi1499@gmail.com>
2020-08-27 01:52:43 +0530

    rotate_array

---
+++ array/Rotate_array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int n,d;
+        cin>>n>>d;
+        int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[(i+n-d)%n];
+        }
+        for(int i=0;i<n;i++)
+        {
+            cout<<a[i]<<" ";
+            
+        }
+        cout<<endl;
+
+	//code
+	return 0;
+}
